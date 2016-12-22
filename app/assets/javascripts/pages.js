92c3ced274aab4dbf4226c8cfa695ee686e468f9
Danish Mughal <mughal.danish@gmail.com>
2016-12-21 20:29:44 -0800

    Filtering food trucks

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,6 +1,8 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
+/* global $ */
+
 
 function changeColors() {
   setTimeout(function() {
@@ -12,6 +14,66 @@ function changeColors() {
       second.style.color = "blue";
     }, 1000);    
 
-
   }, 1000);
 }
+
+function grabData() {
+  var trucksDiv = document.getElementById('trucks');
+  trucksDiv.innerHTML = "<h2>Loading...</h2>";
+
+  $.get("https://data.sfgov.org/resource/jjew-r69b.json", function(response) {
+    var htmlString = '';
+
+    for (var i = 0; i < response.length; i++) {
+      var truck = response[i];
+      htmlString = htmlString + "<div class='food-truck'>";
+      htmlString = htmlString + "<h2 onclick='changeTruckColor(this)'>" + truck.applicant + "</h2>";
+      htmlString = htmlString + "<h3>" + truck.optionaltext + "</h3>";
+      htmlString = htmlString + "<h4>" + truck.location + "</h4>";
+      htmlString = htmlString + "</div>";
+    }
+
+    trucksDiv.innerHTML = htmlString;
+  });
+}
+
+function changeTruckColor(truckDiv) {
+  if (truckDiv.style.color === "red") {
+    truckDiv.style.color = "black";
+  }
+  else {
+    truckDiv.style.color = "red";
+  }
+}
+
+
+function hideTrucks() {
+  var trucks = document.querySelectorAll('.food-truck');
+  for (var i = 0; i < trucks.length; i++) {
+    trucks[i].style.display = 'none';
+  }
+}
+
+function showTrucks() {
+  var trucks = document.querySelectorAll('.food-truck');
+  for (var i = 0; i < trucks.length; i++) {
+    trucks[i].style.display = '';
+  }
+}
+
+function showTacos() {
+  var trucks = document.querySelectorAll('.food-truck');
+  for (var i = 0; i < trucks.length; i++) {
+    var truck = trucks[i];
+
+    if (truck.innerHTML.indexOf("Tacos") !== -1) {
+      truck.style.display = '';
+    }
+    else {
+      truck.style.display = 'none';
+    }
+
+  }
+}
+
+
